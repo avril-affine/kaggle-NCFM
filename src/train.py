b43334c624da39b55d87e3e331eaf195e30b7d94
kkihara <kkihara1@gmail.com>
2017-01-08 15:00:23 -0800

    merged train_folds.py into train.py

--- src/train.py
+++ src/train.py
@@ -1,40 +1,64 @@
 import os
 import sys
-from keras.applications.inception_v3 import InceptionV3
-from keras.models import Model
-from keras.layers import Flatten, Dense, AveragePooling2D
+import argparse
+import subprocess
 from keras.optimizers import Adam
 from keras.callbacks import ModelCheckpoint, EarlyStopping
 from keras.preprocessing.image import ImageDataGenerator
 from my_iterator import MyDirectoryIterator
-from models import classify, localize_classify
 from my_tensorboard import BatchTensorboard
 
 
-HOME_DIR = '/home/panda/Desktop/Projects/fish'
+def run_folds(args):
+    kfolds = args.__dict__.pop('kfolds')
+    data_dir = args.__dict__.pop('data_dir')
+    fold_prefix = args.__dict__.pop('fold_prefix')
+    for fold in xrange(args.kfolds):
+        print '------------------Training Fold %i------------------' % fold
+        fold_dir = os.path.join(data_dir, fold_prefix, str(fold))
+        cmd = ['python', __file__,
+               '--data_dir', fold_dir]
+        for k, v in args.__dict__.iteritems():
+            option = '--' + k
+            if isinstance(v, bool):
+                if v:
+                    cmd += [option]
+            else:
+                if v is not None:
+                    cmd += [option, v]
+        subprocess.call(cmd, shell=True)
 
-LR = 0.000001
-EPOCHS = 100
-BATCH_SIZE = 32
 
-SHEAR = 0.1
-ZOOM = 0.1
-ROTATION = 10.
-SHIFT = 0.1
-FLIP = True
+def main(args):
+    # Model parameters
+    module, function = args.import_model.rsplit('.', 1)
+    import_model = __import__(module, fromlist=[function]).__dict__[function]
+    LR = args.learning_rate
+    EPOCHS = args.epochs
+    BATCH_SIZE = args.batch_size
+    MULTI_OUTPUT = args.multi_output
+    if MULTI_OUTPUT and not args.multi_labels:
+        print '--multi_labels is required if MULTI_OUTPUT is specified'
+        sys.exit(1)
+    MULTI_LABEL_FILE = args.multi_labels
+    EARLY_STOP = args.early_stop
 
-MULTI_OUTPUT = True
+    # Data augmentation parameters
+    SHEAR = args.shear
+    ZOOM = args.zoom
+    ROTATION = args.rotation
+    SHIFT = args.shift
+    FLIP_LR = args.flip_lr
+    FLIP_UD = args.flip_ud
 
-
-def main():
-    data_dir = sys.argv[1]
-    fold = sys.argv[2]
-    if MULTI_OUTPUT:
-        box_file = sys.argv[3]
-    train_dir = os.path.join(data_dir, 'fold_' + fold, 'train')
-    val_dir = os.path.join(data_dir, 'fold_' + fold,  'val')
-    weights_dir = os.path.join(
-        HOME_DIR, 'models/k_fold_low_final_low_lr_early_stop/fold_' + fold)
+    # Directories
+    HOME_DIR = args.home_dir
+    MODEL_NAME = args.model_name
+    data_dir = args.data_dir
+    fold = os.path.basename(os.path.dirname(data_dir))
+    train_dir = os.path.join(data_dir, 'train')
+    val_dir = os.path.join(data_dir, 'val')
+    weights_dir = os.path.join(HOME_DIR, 'models', MODEL_NAME, fold)
 
     train_folders = os.listdir(train_dir)
     train_size = 0
@@ -47,10 +71,7 @@ def main():
         train_size += len(os.listdir(train_folder_path))
         val_size += len(os.listdir(val_folder_path))
 
-    model = localize_classify()
-
-    tensorboard = BatchTensorboard(log_dir=weights_dir, write_graph=False)
-    early_stop = EarlyStopping(patience=7, verbose=1)
+    model = import_model()
 
     adam = Adam(lr=LR)
     if MULTI_OUTPUT:
@@ -58,18 +79,24 @@ def main():
                       loss={'localize': 'mse',
                             'classify': 'categorical_crossentropy'},
                       metrics={'classify': 'accuracy'},
-                      loss_weights={'localize': 0.05,
+                      loss_weights={'localize': args.multi_weight,
                                     'classify': 1.})
     else:
         model.compile(optimizer=adam, loss='categorical_crossentropy',
                       metrics=['accuracy'])
 
-    if not os.path.exists(weights_dir):
-        os.makedirs(weights_dir)
-
+    # Callbacks
     checkpoint = ModelCheckpoint(os.path.join(weights_dir, 'weights.h5'),
                                  verbose=1,
                                  save_best_only=True)
+    tensorboard = BatchTensorboard(log_dir=weights_dir, write_graph=False)
+    callbacks = [checkpoint, tensorboard]
+    if EARLY_STOP:
+        early_stop = EarlyStopping(patience=7, verbose=1)
+        callbacks += [early_stop]
+
+    if not os.path.exists(weights_dir):
+        os.makedirs(weights_dir)
 
     train_gen = ImageDataGenerator(
         rescale=1. / 255,
@@ -81,7 +108,7 @@ def main():
         horizontal_flip=True)
     if MULTI_OUTPUT:
         train_gen = MyDirectoryIterator(
-            box_file,
+            MULTI_LABEL_FILE,
             train_dir,
             train_gen,
             target_size=(299, 299),
@@ -99,7 +126,7 @@ def main():
     val_gen = ImageDataGenerator(rescale=1. / 255)
     if MULTI_OUTPUT:
         val_gen = MyDirectoryIterator(
-            box_file,
+            MULTI_LABEL_FILE,
             val_dir,
             val_gen,
             target_size=(299, 299),
@@ -121,8 +148,62 @@ def main():
         validation_data=val_gen,
         nb_val_samples=val_size,
         verbose=1,
-        callbacks=[checkpoint, tensorboard, early_stop])
+        callbacks=callbacks)
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        description='Train a keras model with kfolds or a single run.')
+    parser.add_argument('--home_dir', required=True,
+        help='Path to the root directory of the project')
+    parser.add_argument('--data_dir', required=True,
+        help='Path to a data directory. If kfolds are specified, '
+        'the directory should contain a directory for each fold 0 to k-1, '
+        'else it should contain a train and val folder.')
+    parser.add_argument('--model_name', required=True,
+        help='Name of the model. This will also be the name of the directory '
+             'that stores the model weights and run data.')
+    parser.add_argument('--import_model', required=True,
+        help='Model to import and run in the form of '
+             'path.to.script.function: where `path.to.script` is the module '
+             'to import and `function` is the function within the module.')
+    parser.add_argument('--kfolds', type=int, default=0,
+        help='Number of folds to run the model on. The directory structure '
+             'must be setup before running this file.')
+    parser.add_argument('--fold_prefix', default='fold_',
+        help='Prefix for each fold directory.')
+    parser.add_argument('--learning_rate', type=float, default=0.0001,
+        help='Learning rate of the model.')
+    parser.add_argument('--epochs', type=int, default=10,
+        help='Number of epochs to run the model.')
+    parser.add_argument('--batch_size', type=int, default=32,
+        help='Batch size for the model.')
+    parser.add_argument('--early_stop', type=int, default=0,
+        help='Number of epochs without improvement to wait before stopping. '
+             'Default to run all epochs.')
+    parser.add_argument('--shear', type=float, default=0.,
+        help='Data augmentation: shear intensity in radians.')
+    parser.add_argument('--zoom', type=float, default=0.,
+        help='Data augmentation: range for random zoom.')
+    parser.add_argument('--rotation', type=float, default=0.,
+        help='Data augmentation: range for random rotation in degrees.')
+    parser.add_argument('--shift', type=float, default=0.,
+        help='Data augmentation: range for random shifts in x/y directions.')
+    parser.add_argument('--flip_lr', action='store_true',
+        help='Data augmentation: whether to apply random horizontal flips.')
+    parser.add_argument('--flip_ud', action='store_true',
+        help='Data augmentation: whether to apply random vertical flips.')
+    parser.add_argument('--multi_output', action='store_true',
+        help='Give the model multiple outputs, currently bounding boxes.')
+    parser.add_argument('--multi_labels', default=None,
+        help='Required if multi_output is set. Path to a json file containing '
+             'maps from img to second label, currently bounding boxes.')
+    parser.add_argument('--multi_weight', type=float, default=1.,
+        help='Required if multi_output is set. Weight for the second output '
+             'relative to the logloss output.')
+    args = parser.parse_args()
+
+    if args.kfolds:
+        run_folds(args)
+    else:
+        main(args)
