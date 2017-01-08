833d050a02fea4f9b9888c2571e3abde77aef038
kkihara <kkihara1@gmail.com>
2017-01-08 10:08:40 -0800

    switched train_folds script to python

--- src/train_folds.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-DATA_DIR="/home/panda/Desktop/Projects/fish/data/train_folds"
-
-for i in `seq 0 9`;
-do
-    echo "------------------Training Fold $i------------------"
-    python train.py $DATA_DIR $i
-done
