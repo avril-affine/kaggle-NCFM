57c3902eb441ac81fe663de4d10105f368d68f16
kkihara <kkihara1@gmail.com>
2017-02-05 17:36:09 -0800

    tested

--- src/inspect_localizer.py
+++ src/inspect_localizer.py
@@ -2,32 +2,40 @@ import os
 import argparse
 import numpy as np
 from keras.models import load_model
+from keras.preprocessing.image import load_img
 from PIL import ImageDraw
+from my_keras_model import Model
 
 
 def main(args):
     model = load_model(args.import_model)
+    output = model.get_layer('localize').output
+    model = Model(model.input, output)
 
     val_dir = args.val_dir
     val_imgs = os.listdir(val_dir)
-    np.random.shuffle(va_imgs)
+    np.random.shuffle(val_imgs)
 
     for val_img in val_imgs:
         img = load_img(os.path.join(val_dir, val_img), target_size=(299, 299))
 
-        pred = model.predict(img)
+        pred = model.predict(np.expand_dims(np.array(img), axis=0))[0]
 
         draw = ImageDraw.Draw(img)
         draw.rectangle((pred[0],
                         pred[1],
                         pred[0] + pred[2],
-                        pred[1] + pred[3]))
+                        pred[1] + pred[3]),
+                       fill=(255, 0, 0))
 
         img.show()
+        print pred[0], pred[1], pred[0] + pred[2], pred[1] + pred[3]
         raw_input('Press Enter to continue...')     # wait for input
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Inspect images from localizer model.')
     parser.add_argument('--import_model', required=True,
         help='Path to localizer .h5 weights file.')
     parser.add_argument('--val_dir', required=True,
