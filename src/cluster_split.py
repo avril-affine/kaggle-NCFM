f51d69ee5dc0c51bb89519780194e8b6134aa72e
kkihara <kkihara1@gmail.com>
2017-02-05 17:36:32 -0800

    fixed bugs

--- src/cluster_split.py
+++ src/cluster_split.py
@@ -1,8 +1,8 @@
 import os
 import numpy as np
-import pandas as pd
+import cPickle as pickle
 from PIL import Image
-from sklearn.cluster import KMeans
+from sklearn.cluster import MiniBatchKMeans
 
 
 def load_data():
@@ -13,36 +13,38 @@ def load_data():
     data = []
     label = []
     label_name = []
-    for i, folder in enumerate(os.listdir(data_dir)):
-        if folder.startswith('.'):
-            continue
-
-        for img_path in os.listdir(os.path.join(data_dir, folder)):
-            if img_path.startswith('.'):
+    folders = [x for x in os.listdir(data_dir) if not x.startswith('.')]
+    for i, folder in enumerate(folders):
+        for img_name in os.listdir(os.path.join(data_dir, folder)):
+            if img_name.startswith('.'):
                 continue
 
+            img_path = os.path.join(data_dir, folder, img_name)
             img = Image.open(img_path)
-            img = img.resize((299, 299)).flatten()
+            img = np.array(img.resize((299, 299)))
+            img = img.flatten()
             data.append(img)
             label.append(i)
             label_name.append(folder)
 
-    df = pd.DataFrame({'data': data, 'label': label, 'label_name': label_name})
-    return df
+    return np.array(data), np.array(label), np.array(label_name)
 
 
 def main():
-    df = load_data()
+    data, label, label_name = load_data()
 
-    model = KMeans(n_clusters=12)
-    preds = model.fit_predict(df['data'].values)
+    model = MiniBatchKMeans(n_clusters=12, verbose=True)
+    preds = model.fit_predict(data)
 
-    clusters = [[0 for _ in xrange(12)] for _ in xrange(12)]
-    for label, pred in zip(df['label'].values, preds):
+    clusters = [[0 for _ in xrange(8)] for _ in xrange(12)]
+    for label, pred in zip(label, preds):
         clusters[pred][label] += 1
 
     for i in xrange(12):
         print i, clusters[i]
+
+    with open('kmeans.pkl', 'w') as f:
+        pickle.dump(model, f)
 
 
 if __name__ == '__main__':
