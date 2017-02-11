65080aa9464621abf88285489e8201266e5ef689
kkihara <kkihara1@gmail.com>
2017-02-11 13:00:20 -0500

    cache data

--- src/cluster_split.py
+++ src/cluster_split.py
@@ -1,5 +1,6 @@
 import os
 import numpy as np
+import h5py
 import cPickle as pickle
 from PIL import Image
 from sklearn.cluster import MiniBatchKMeans
@@ -13,10 +14,13 @@ def load_data():
     data = []
     label = []
     label_name = []
-    folders = [x for x in os.listdir(data_dir) if not x.startswith('.')]
+    folders = [x for x in os.listdir(data_dir)
+               if not x.startswith('.')
+               and os.path.isdir(os.path.join(data_dir, x))]
     for i, folder in enumerate(folders):
-        for img_name in os.listdir(os.path.join(data_dir, folder)):
-            if img_name.startswith('.'):
+        img_folder = os.path.join(data_dir, folder)
+        for img_name in os.listdir(img_folder):
+            if not img_name.endswith('.jpg'):
                 continue
 
             img_path = os.path.join(data_dir, folder, img_name)
@@ -31,10 +35,24 @@ def load_data():
 
 
 def main():
-    data, label, label_name = load_data()
+    if not os.path.exists('data.h5'):
+        data, label, label_name = load_data()
 
+        with open ('data.h5', 'w') as hf:
+            hf.create_dataset('data', data=data)
+            hf.create_dataset('label', data=label)
+            hf.create_dataset('label_name', data=label_name)
+    else:
+        with open('data.h5', 'r') as hf:
+            data = np.array(hf.get('data')[:])
+            label = np.array(hf.get('label')[:])
+            label_name = np.array(hf.get('label_name')[:])
+
+    print data.shape
     model = MiniBatchKMeans(n_clusters=12, verbose=True)
     preds = model.fit_predict(data)
+    with open('kmeans.pkl', 'w') as f:
+        pickle.dump(model, f)
 
     clusters = [[0 for _ in xrange(8)] for _ in xrange(12)]
     for label, pred in zip(label, preds):
@@ -43,8 +61,7 @@ def main():
     for i in xrange(12):
         print i, clusters[i]
 
-    with open('kmeans.pkl', 'w') as f:
-        pickle.dump(model, f)
+    return clusters
 
 
 if __name__ == '__main__':
